2cfedfc2cd923444f8e80f03d37f0f1009ad6e72
Jarno Bakker <jarno.bakker@di.huc.knaw.nl>
2024-08-20 14:07:42 +0200

    cleanup

--- src/browser/src/index.tsx
+++ src/browser/src/index.tsx
@@ -1,10 +1,8 @@
 import React, {createElement} from 'react';
 import ReactDOM from 'react-dom/client';
-import logo from './logo.svg';
 import './assets/css/sport_style.css';
 import {
     App,
-    PageHeader,
     Search,
     Detail as BrowserDetail,
     createSearchLoader,
@@ -12,22 +10,10 @@ import {
     searchUtils,
     SearchParams
 } from '@knaw-huc/browser-base-react';
-import {createBrowserRouter, createHashRouter, RouteObject, RouterProvider} from 'react-router-dom';
+import {createHashRouter, RouteObject, RouterProvider} from 'react-router-dom';
 import Facets from "./components/facets";
 import ListItem from "./components/listItem";
 import {Detail} from "./components/detail";
-import {Header} from "./components/pageHeader";
-import {Literatuur} from "./components/literatuur";
-import {Colofon} from "./components/colofon";
-import {DatabaseInfo} from "./components/databaseInfo";
-import {Gymnastiek} from "./components/sport/gymnastiek";
-import {Hockey} from "./components/sport/hockey";
-import {Korfbal} from "./components/sport/korfbal";
-import {Schaken} from "./components/sport/schaken";
-import {Tennis} from "./components/sport/tennis";
-import {Voetbal} from "./components/sport/voetbal";
-import {Home} from "./components/home";
-import {Inleiding} from "./components/inleiding";
 import {BASE_URL} from "./misc/config";
 
 const header = <></>
@@ -38,9 +24,7 @@ const routeObject: RouteObject = {
     path: '/',
     element: <App header={header}/>,
     children: [
-        // {index: true, element: <Home/> },
         {
-            // path:"/search",
             index: true,
             loader: async ({request}) => searchLoader(new URL(request.url).searchParams),
             element: <Search title={title} pageLength={30} withPaging={true}
@@ -51,16 +35,6 @@ const routeObject: RouteObject = {
             loader: async ({params}) => detailLoader(params.id as string),
             element: <BrowserDetail title={title} updateDocumentTitle={false} DetailComponent={Detail}/>
         },
-        // { path: '/literatuur', element: <Literatuur/> },
-        // { path: '/colofon', element: <Colofon/> },
-        // { path: '/gymnastiek', element: <Gymnastiek/>},
-        // { path: '/hockey', element: <Hockey/>},
-        // { path: '/korfbal', element: <Korfbal/>},
-        // { path: '/schaken', element: <Schaken/>},
-        // { path: '/tennis', element: <Tennis/>},
-        // { path: '/voetbal', element: <Voetbal/>},
-        // { path: '/inleiding', element: <Inleiding/>},
-        // { path: '/database', element: <DatabaseInfo/>}
     ]
 };
 
